e75c79db0ccbc9e24d62d5302d7220185818861e
Reid Carlberg <reid.carlberg@reidcarlbe-ltm.internal.salesforce.com>
2015-02-20 22:07:36 -0600

    handlers

--- parenting.js
+++ parenting.js
@@ -1,23 +1,30 @@
 /*
 	parenting
 	v 0.0.6
-	@ReidCarlberg - reid.carlberg@gmail.com
+	@ReidCarlberg
+	reid.carlberg@gmail.com
 	February 18, 2015
 */
 
-var Say = require('say');
-var readline = require('readline');
-var _ = require('lodash');
+var Say = require('say'),
+	readline = require('readline'),
+	_ = require('lodash'),
+	fs = require('fs'),
+	js = require('jsonfile'),
+	util = require('util');
 
 var rl = readline.createInterface({
 	  input: process.stdin,
 	  output: process.stdout
 	});
 
-var Parenting = {
+var configFile = "./parenting.config.json";
 
+var defaultConfig = {
+	voice: "Alex",
 	child: "DefaultName",
-
+	prompt: "Yes?",
+	mute: false,
 	prompts: [
 		{"id": "1", "prompt": "have you used your napkin?"},
 		{"id": "2", "prompt": "eat over your plate."},  	
@@ -27,14 +34,59 @@ var Parenting = {
 		{"id": "6", "prompt": "sit in your seat."},
 		{"id": "7", "prompt": "use your fork."},
 		{"id": "8", "prompt": "clean your room."}
-	],
+	]
+};
+
+var Parenting = {
+
+	configFile: "./parenting.config.json",
+
+	config: {},
 
 	sentenceQueue: [],
 
 	isSpeaking: false,
 
+	handlers: {
+		t: function() {
+			console.log("test!");
+		},
+
+		q: function() {
+			console.log('Goodbye!');
+			that.saveConfig();
+			rl.close();		
+			process.exit(1);	
+		},
+
+		r: function() {
+			console.log('Resetting config to defaults.');
+			that.config = defaultConfig;
+		},
+
+		m: function() {
+			if (that.config.mute === false) {
+				console.log("muted!");
+				that.config.mute = true;
+			} else {
+				console.log("unmuted!");
+				that.config.mute = false;
+			}
+		},
+
+		c: function(data) {
+			var newChild = data.substring(data.indexOf(' ')+1);
+			console.log("Setting new child name to " + newChild);
+			that.config.child = newChild;
+		},
+
+		v: function(data) {
+
+		}
+	},
+
 	handleSentence: function() {
-		if (this.sentenceQueue.length == 0 || this.isSpeaking === true) {
+		if (this.sentenceQueue.length == 0 || this.isSpeaking === true || this.config.mute === true) {
 			return;
 		}
 		isSpeaking = true;
@@ -48,20 +100,35 @@ var Parenting = {
 
 	ask: function() {
 		that = this;
-		rl.question("Yes? ", function(answer) {
-			var prompt = _.find(that.prompts, { 'id': answer });
+		rl.question(that.config.prompt + " ", function(answer) {
+			var handled = false;
+			var prompt = _.find(that.config.prompts, { 'id': answer });
 
 			if (prompt) {
-				that.sentenceQueue.push(that.child + ", " + prompt.prompt);  	
-				that.handleSentence();
-			} else if (answer === "q") {
-				rl.close();
-				return;
-			} else {
-				for (i = 0; i < that.prompts.length; i++) {
-					console.log(that.prompts[i].id + ". " + that.prompts[i].prompt);
+				var sentenceTemp = that.config.child + ", " + prompt.prompt;
+				if (that.config.mute === false) {
+					that.sentenceQueue.push(sentenceTemp);					
+					that.handleSentence();	
+				} else {
+					console.log("[muted] " + sentenceTemp);
 				}
-				console.log("(q)uit");
+				handled = true;
+			}
+
+			if (!handled && answer.match(/^\\.*$/)) {
+				var command = answer.substring(1,2);
+				var handler = that.handlers[command];
+				if (handler) {
+					handler.call(that, answer);
+					handled = true;
+				}
+			} 
+
+			if (!handled) {
+				for (i = 0; i < that.config.prompts.length; i++) {
+					console.log(that.config.prompts[i].id + ". " + that.config.prompts[i].prompt);
+				}
+				console.log("\\(q)uit (m)ute (v)oice (c)hild (r)eset (t)est");
 			}
 
 			that.ask();
@@ -69,7 +136,20 @@ var Parenting = {
 		});
 	},
 
+	readConfig: function() {
+		if (fs.existsSync(configFile)) {
+			this.config = js.readFileSync(configFile);
+		} else {
+			this.config = defaultConfig;
+		}
+	},
+
+	saveConfig: function() {
+		js.writeFileSync(configFile, this.config);
+	},
+
 	start: function() {
+		this.readConfig();
 		this.ask();
 	}	
 
